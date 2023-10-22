1f2c2579d7c1690d03f169b160a623cfa6faad57
Krishna Agrawal <krishnaagrawal2304@gmail.com>
2023-10-22 16:06:33 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Router from './router/Router'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <>
       <Router />
+      <Footer/>
     </>
   )
 }
